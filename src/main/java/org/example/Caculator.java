b588180b0deff745e0ca9fe1dbb3e32f3e9c9372
unknown <your@email.com>
2025-04-14 17:29:07 +0700

    first

---
+++ src/main/java/org/example/Caculator.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Caculator implements ICaculator {
+
+
+    public Integer sum(Integer a, Integer b) {
+        return 0;
+    }
+
+
+    public Integer sub(Integer a, Integer b) {
+        return 0;
+    }
+}
